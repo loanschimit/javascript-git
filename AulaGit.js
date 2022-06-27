45935f6b602dabf7836d580c09ac1f114e01702b
Loan <loan.v.schimit1@gmail.com>
2022-06-27 12:04:06 -0300

    primeiro commit

---
+++ AulaGit.js
@@ -0,0 +1,19 @@
+export default function GitInit() {
+  /* Git */
+  // Git| Sistema de controle de versões.
+  // Facilita o trabalho em equipe e o controle de mudanças entre arquivos e diretórios.
+  // Github| Plataforma online de hospedagem para repositórios Git.
+  // Existem outras como GitLab e Bitbucket.
+
+  /* Git Setup */
+  // Instalar o Git| https://git-scm.com/
+  // Configurar Nome| $ git config --global user.name "Seu Nome"
+  // Configurar Email| $ git config --global user.email "email@gmail.com"
+
+  /* Git Comandos */
+  // $ git init| Inicia um repositório
+  // $ git add style.css| Adiciona o arquivo style.css ao index do git. 
+  // Com o $ git add -A adiciona todos os arquivos
+  // $ git status| Mostra os arquivos que tiveram mudanças.
+  // $ git commit -m 'Descrição'| Irá fazer o commit do código adicionado com uma mensagem.
+}
