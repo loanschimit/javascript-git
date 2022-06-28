c489c179ba57901442aa2df20b2f43ef8e47f284
Loan <loan.v.schimit1@gmail.com>
2022-06-27 23:23:24 -0300

    coments

--- AulaGit.js
+++ AulaGit.js
@@ -24,4 +24,9 @@ export default function GitInit() {
   // Branch|Uma das principais vantagens do git é a possibilidade de criarmos 'ramificações'. Assim podemos trabalhar em funcionalidades adicionais para um projeto, sem modificarmos o 'ramificação principal, o master.
   // $ git branch nomebranch| Toda vez que formos adicionar uma nova funcionalidade, devemos iniciar criando um novo branch ao invés de fazermos alterações direto no master. O que for modificado no branch não afetara o master.
   // $ git checkout nomebranch| Ira mudar de branch. Podemos usar o atalho $ git checkout -b novobranch, assim ele cria e muda de branch ao mesmo tempo.
+  
+  /* Lidando com Pull Request */
+  // No Github| Agora você possui um novo branch no github e pode fazer o pull request(juntar ao main).
+  // Compare e Pull Request| Pode adicionar comentários. Create Pull Request.
+  // Merge Pull Request| Geralmente é o lider do projeto/responsável por fazer o review do seu código. Pode deletar o branch após o merge com o Main.
 }
